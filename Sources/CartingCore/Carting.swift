73c50473411e3da71666646c07bf6239525e5cef
Artem Novichkov <novichkoff93@gmail.com>
2018-09-20 18:21:51 +0600

    Move update logic to framework service

--- Sources/CartingCore/Carting.swift
+++ Sources/CartingCore/Carting.swift
@@ -6,14 +6,9 @@ import Foundation
 
 public final class Carting {
 
-    enum Keys {
-        static let carthageScript = "\"/usr/local/bin/carthage copy-frameworks\""
-    }
-
     private let arguments: [String]
 
-    private let projectService = ProjectService()
-    private lazy var frameworkInformationService = FrameworkInformationService()
+    private lazy var frameworkInformationService: FrameworkInformationService = .init()
 
     public init(arguments: [String] = CommandLine.arguments) {
         self.arguments = arguments
@@ -31,77 +26,9 @@ public final class Carting {
         case .help:
             print(Arguments.description)
         case let .script(name: name):
-            try updateScript(withName: name, path: arguments.path)
+            try frameworkInformationService.updateScript(withName: name, path: arguments.path)
         case .list:
-            frameworkInformationService.printFrameworksInformation()
-        }
-    }
-
-    private func updateScript(withName scriptName: String, path: String?) throws {
-        let project = try projectService.project(path)
-
-        var projectHasBeenUpdated = false
-
-        try project.targets.forEach { target in
-            let frameworkBuildPhase = target.body.buildPhases.first { $0.name == "Frameworks" }
-            let frameworkScript = project.frameworkScripts.first { $0.identifier == frameworkBuildPhase?.identifier }
-            guard let script = frameworkScript else {
-                return
-            }
-            let linkedCarthageDynamicFrameworkNames = try frameworkInformationService.frameworksInformation()
-                .filter { information in
-                    information.linking == .dynamic && script.body.files.contains { $0.name == information.name }
-                }
-                .map { $0.name }
-
-            let carthageBuildPhase = target.body.buildPhases.first { $0.name == scriptName }
-            let carthageScript = project.scripts.first { $0.identifier == carthageBuildPhase?.identifier }
-
-            let inputPathsString = projectService.pathsString(forFrameworkNames: linkedCarthageDynamicFrameworkNames,
-                                                              type: .input)
-            let outputPathsString = projectService.pathsString(forFrameworkNames: linkedCarthageDynamicFrameworkNames,
-                                                               type: .output)
-
-            if let carthage = carthageScript {
-                var scriptHasBeenUpdated = false
-                if carthage.body.inputPaths != inputPathsString {
-                    carthage.body.inputPaths = inputPathsString
-                    scriptHasBeenUpdated = true
-                }
-                if carthage.body.outputPaths != outputPathsString {
-                    carthage.body.outputPaths = outputPathsString
-                    scriptHasBeenUpdated = true
-                }
-                if carthage.body.shellScript != Keys.carthageScript {
-                    carthage.body.shellScript = Keys.carthageScript
-                    scriptHasBeenUpdated = true
-                }
-                if scriptHasBeenUpdated {
-                    projectHasBeenUpdated = true
-                    print("✅ Script \"\(scriptName)\" in target \"\(target.name)\" was successfully updated.")
-                }
-            }
-            else if linkedCarthageDynamicFrameworkNames.count > 0 {
-                let body = ScriptBody(inputPaths: inputPathsString,
-                                      name: scriptName,
-                                      outputPaths: outputPathsString,
-                                      shellScript: Keys.carthageScript)
-
-                let identifier = String.randomAlphaNumericString(length: 24)
-                let script = Script(identifier: identifier, name: scriptName, body: body)
-                let buildPhase = BuildPhase(identifier: identifier, name: scriptName)
-                project.scripts.append(script)
-                target.body.buildPhases.append(buildPhase)
-                print("✅ Script \(scriptName) was successfully added to \(target.name) target.")
-                projectHasBeenUpdated = true
-            }
-        }
-
-        if projectHasBeenUpdated {
-            try projectService.update(project)
-        }
-        else {
-            print("🤷‍♂️ Nothing to update.")
+            try frameworkInformationService.printFrameworksInformation()
         }
     }
 }
@@ -110,9 +37,9 @@ enum MainError: Swift.Error {
     case noScript(name: String)
 }
 
-extension MainError: LocalizedError {
+extension MainError: CustomStringConvertible {
 
-    var errorDescription: String? {
+    var description: String {
         switch self {
         case .noScript(name: let name): return "Can't find script with name \(name)"
         }
--- Sources/CartingCore/Services/FrameworkInformationService.swift
+++ Sources/CartingCore/Services/FrameworkInformationService.swift
@@ -6,7 +6,11 @@ import Files
 import ShellOut
 import Foundation
 
-class FrameworkInformationService {
+final class FrameworkInformationService {
+
+    enum Keys {
+        static let carthageScript = "\"/usr/local/bin/carthage copy-frameworks\""
+    }
 
     var path: String?
 
@@ -17,7 +21,80 @@ class FrameworkInformationService {
         return FileSystem().currentFolder
     }
 
+    private lazy var projectService: ProjectService = .init()
+
     // MARK: - Lifecycle
+
+    func updateScript(withName scriptName: String, path: String?) throws {
+        let project = try projectService.project(path)
+
+        var projectHasBeenUpdated = false
+
+        try project.targets.forEach { target in
+            let frameworkBuildPhase = target.body.buildPhases.first { $0.name == "Frameworks" }
+            let frameworkScript = project.frameworkScripts.first { $0.identifier == frameworkBuildPhase?.identifier }
+            guard let script = frameworkScript else {
+                return
+            }
+            let linkedCarthageDynamicFrameworkNames = try frameworksInformation()
+                .filter { information in
+                    information.linking == .dynamic && script.body.files.contains { $0.name == information.name }
+                }
+                .map { $0.name }
+
+            let carthageBuildPhase = target.body.buildPhases.first { $0.name == scriptName }
+            let carthageScript = project.scripts.first { $0.identifier == carthageBuildPhase?.identifier }
+
+            let inputPaths = projectService.paths(forFrameworkNames: linkedCarthageDynamicFrameworkNames,
+                                                  type: .input)
+            let outputPaths = projectService.paths(forFrameworkNames: linkedCarthageDynamicFrameworkNames,
+                                                   type: .output)
+
+            let inputPathsString = projectService.description(forPaths: inputPaths)
+            let outputPathsString = projectService.description(forPaths: outputPaths)
+
+            if let carthage = carthageScript {
+                var scriptHasBeenUpdated = false
+                if carthage.body.inputPaths != inputPathsString {
+                    carthage.body.inputPaths = inputPathsString
+                    scriptHasBeenUpdated = true
+                }
+                if carthage.body.outputPaths != outputPathsString {
+                    carthage.body.outputPaths = outputPathsString
+                    scriptHasBeenUpdated = true
+                }
+                if carthage.body.shellScript != Keys.carthageScript {
+                    carthage.body.shellScript = Keys.carthageScript
+                    scriptHasBeenUpdated = true
+                }
+                if scriptHasBeenUpdated {
+                    projectHasBeenUpdated = true
+                    print("✅ Script \"\(scriptName)\" in target \"\(target.name)\" was successfully updated.")
+                }
+            }
+            else if linkedCarthageDynamicFrameworkNames.isEmpty {
+                let body = ScriptBody(inputPaths: inputPathsString,
+                                      name: scriptName,
+                                      outputPaths: outputPathsString,
+                                      shellScript: Keys.carthageScript)
+
+                let identifier = String.randomAlphaNumericString(length: 24)
+                let script = Script(identifier: identifier, name: scriptName, body: body)
+                let buildPhase = BuildPhase(identifier: identifier, name: scriptName)
+                project.scripts.append(script)
+                target.body.buildPhases.append(buildPhase)
+                print("✅ Script \(scriptName) was successfully added to \(target.name) target.")
+                projectHasBeenUpdated = true
+            }
+        }
+
+        if projectHasBeenUpdated {
+            try projectService.update(project)
+        }
+        else {
+            print("🤷‍♂️ Nothing to update.")
+        }
+    }
 
     func frameworksInformation() throws -> [FrameworkInformation] {
         let frameworkFolder = try projectFolder.subfolder(atPath: "Carthage/Build/iOS")
@@ -25,22 +102,19 @@ class FrameworkInformationService {
         return try frameworks.map(information)
     }
 
-    func printFrameworksInformation() {
-        do {
-            let informations = try frameworksInformation()
-            informations.forEach { information in
-                let description = [information.name, information.linking.rawValue].joined(separator: "\t\t") +
-                    "\t" +
-                    information.architectures.map { $0.rawValue }.joined(separator: ", ")
-                print(description)
-            }
-        }
-        catch {
-            print(error)
+    func printFrameworksInformation() throws {
+        let informations = try frameworksInformation()
+        informations.forEach { information in
+            let description = [information.name, information.linking.rawValue].joined(separator: "\t\t") +
+                "\t" +
+                information.architectures.map { $0.rawValue }.joined(separator: ", ")
+            print(description)
         }
     }
 
-    func information(for framework: Folder) throws -> FrameworkInformation {
+    // MARK: - Private
+
+    private func information(for framework: Folder) throws -> FrameworkInformation {
         let path = framework.path + framework.nameExcludingExtension
         let fileOutput = try shellOut(to: "file", arguments: [path.quotify])
         let lipoOutput = try shellOut(to: "lipo", arguments: ["-info", path.quotify])
@@ -48,28 +122,6 @@ class FrameworkInformationService {
         return FrameworkInformation(name: framework.name,
                                     architectures: architectures(fromOutput: rawArchitectures),
                                     linking: linking(fromOutput: fileOutput))
-    }
-
-    private func convertFrameworkToStatic(withName name: String) throws {
-        let frameworkFolder = try projectFolder.subfolder(atPath: "Carthage/Build/iOS/\(name).framework")
-        let configPath = "/tmp/static.xcconfig"
-        let configFile = try FileSystem().createFile(at: configPath)
-        let content = "MACH_O_TYPE = staticlib"
-        try configFile.write(string: content)
-        setenv("XCODE_XCCONFIG_FILE", configPath, 1)
-
-//        let output = try shellOut(to: "export", arguments: ["XCODE_XCCONFIG_FILE=\"\(configPath)\""])
-//        print(output)
-        try shellOut(to: "/usr/local/bin/carthage build", arguments: [frameworkFolder.nameExcludingExtension])
-
-//        let process = Process()
-//        process.environment = ["XCODE_XCCONFIG_FILE": configPath]
-//        process.launchPath = "/bin/bash"
-//        process.arguments = ["-c", "usr/local/bin/carthage build", frameworkFolder.nameExcludingExtension]
-//        process.launch()
-//        process.waitUntilExit()
-
-        print("Done!")
     }
 }
 
@@ -101,5 +153,5 @@ func linking(fromOutput output: String) -> FrameworkInformation.Linking {
 }
 
 func architectures(fromOutput output: String) -> [FrameworkInformation.Architecture] {
-    return output.components(separatedBy: " ").compactMap { FrameworkInformation.Architecture(rawValue: $0) }
+    return output.components(separatedBy: " ").compactMap(FrameworkInformation.Architecture.init)
 }
