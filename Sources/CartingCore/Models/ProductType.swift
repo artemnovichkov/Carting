cc8dcb6ef3e6fd7331e8952cb27fba437ae1cd8a
Alexey Korolev <alphatroya@gmail.com>
2018-11-07 16:10:00 +0300

    Trim \" symbols in product type check

--- Sources/CartingCore/Models/ProductType.swift
+++ Sources/CartingCore/Models/ProductType.swift
@@ -6,20 +6,16 @@ import Foundation
 
 struct ProductType {
     private let value: String
+
     init(value: String) {
         self.value = value
     }
 
     var description: String {
-        return self.value
+        return value
     }
 
     var isApplication: Bool {
-        switch self.value {
-        case "\"com.apple.product-type.application\"":
-            return true
-        default:
-            return false
-        }
+        return value.replacingOccurrences(of: "\"", with: "") == "com.apple.product-type.application"
     }
 }
