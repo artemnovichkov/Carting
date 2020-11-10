9c3618ca978fe0397d0980ffe63d0ef03276c376
Artem Novichkov <novichkoff93@gmail.com>
2020-11-10 10:30:22 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .package(url: "https://github.com/JohnSundell/Files.git", from: "3.0.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
-        .package(url: "https://github.com/tuist/xcodeproj.git", .upToNextMajor(from: "7.0.0"))
+        .package(url: "https://github.com/tuist/xcodeproj.git", .exact("7.17.0"))
     ],
     targets: [
         .target(name: "Carting", dependencies: ["CartingCore", "ArgumentParser"]),
